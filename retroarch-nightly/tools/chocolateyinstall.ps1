9f1236a0b0951acacfc342b3517d17137835b4ae
Ömercan Kömür <fpscan@gmail.com>
2024-06-24 21:40:03 +0300

    update

--- retroarch-nightly/tools/chocolateyinstall.ps1
+++ retroarch-nightly/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageName = 'retroarch-nightly'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64       = 'https://buildbot.libretro.com/nightly/windows/x86_64/RetroArch.7z'
 $url32       = 'https://buildbot.libretro.com/nightly/windows/x86/RetroArch.7z'
-$checksum64  = ''
-$checksum32  = ''
+$checksum64  = 'ACTUAL_CHECKSUM_64'
+$checksum32  = 'ACTUAL_CHECKSUM_32'
 $checksumType = 'sha256'
 
 $packageArgs = @{
