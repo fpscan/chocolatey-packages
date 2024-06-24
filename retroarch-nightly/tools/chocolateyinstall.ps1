fd83e66ebf9509816e1c28432d9c07693547ef59
Ömercan Kömür <fpscan@gmail.com>
2024-06-24 19:42:30 +0300

    nightly added + gh actions

---
+++ retroarch-nightly/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName = 'retroarch-nightly'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64       = 'https://buildbot.libretro.com/nightly/windows/x86_64/RetroArch.7z'
+$url32       = 'https://buildbot.libretro.com/nightly/windows/x86/RetroArch.7z'
+$checksum64  = ''
+$checksum32  = ''
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+  url           = $url32
+  checksum64    = $checksum64
+  checksum      = $checksum32
+  checksumType64= $checksumType
+  checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
